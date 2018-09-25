9bfd7d71f8946cf706fd2e361bc441e9348702dd
Luana Ferreira do Nascimento <lluana13@hotmail.com>
2018-09-25 14:52:57 -0300

    Falta comentar o js e testar em um pc com camera

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,23 +5,34 @@ navigator.mediaDevices.getUserMedia({ video: true, audio: false })
         video.play();
     })
     .catch(function(err) {
-        console.log("An error occurred! " + err);
+        alert("Ocorreu um erro! " + err);
     });
 
-let canvasFrame = document.getElementById("canvasFrame"); // canvasFrame is the id of <canvas>
-let context = canvasFrame.getContext("2d");
-let src = new cv.Mat(height, width, cv.CV_8UC4);
-let dst = new cv.Mat(height, width, cv.CV_8UC1);
+let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+let cap = new cv.VideoCapture(video);
+
 const FPS = 30;
 function processVideo() {
-    let begin = Date.now();
-    context.drawImage(video, 0, 0, width, height);
-    src.data.set(context.getImageData(0, 0, width, height).data);
-    cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-    cv.imshow("canvasOutput", dst); // canvasOutput is the id of another <canvas>;
-    // schedule next one.
-    let delay = 1000/FPS - (Date.now() - begin);
-    setTimeout(processVideo, delay);
-}
-// schedule first one.
+    try {
+        if (!streaming) {
+            // clean and stop.
+            src.delete();
+            dst.delete();
+            return;
+        }
+        let begin = Date.now();
+        // start processing.
+        cap.read(src);
+        cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+        cv.imshow('canvasOutput', dst);
+        // schedule the next one.
+        let delay = 1000/FPS - (Date.now() - begin);
+        setTimeout(processVideo, delay);
+    } catch (err) {
+        utils.printError(err);
+    }
+};
+
+// schedule the first one.
 setTimeout(processVideo, 0);
