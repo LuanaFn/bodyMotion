dbe6cc358aff75960302089249104bded9ad41ac
Luana Nascimento <lluana13@hotmail.com>
2018-10-12 18:09:38 -0300

    separar rota e controller

---
+++ app/server/controllers/UsuarioController.js
@@ -0,0 +1,186 @@
+
+var CT = require('./../modules/country-list');
+var AM = require('./../modules/account-manager');
+var EM = require('./../modules/email-dispatcher');
+
+module.exports = {
+
+    // main login page //
+	main_get : function(req, res){
+	// check if the user's credentials are saved in a cookie //
+		if (req.cookies.user == undefined || req.cookies.pass == undefined){
+			res.render('login', { title: 'Olá - Por favor faça login' });
+		}	else{
+	// attempt automatic login //
+			AM.autoLogin(req.cookies.user, req.cookies.pass, function(o){
+				if (o != null){
+				    req.session.user = o;
+					res.redirect('/home');
+				}	else{
+					res.render('login', { title: 'Olá - Por favor faça login' });
+				}
+			});
+		}
+	},
+	
+	main_post : function(req, res) {
+		AM.manualLogin(req.body['user'], req.body['pass'], function(e, o){
+			if (!o){
+				res.status(400).send(e);
+			}	else{
+				req.session.user = o;
+				if (req.body['remember-me'] == 'true'){
+					res.cookie('user', o.user, { maxAge: 900000 });
+					res.cookie('pass', o.pass, { maxAge: 900000 });
+				}
+				res.status(200).send(o);
+			}
+		});
+	},
+	
+// logged-in user homepage //
+	
+	home_get: function(req, res) {
+		if (req.session.user == null){
+	// if user is not logged-in redirect back to login page //
+			res.redirect('/');
+		}	else{
+			res.render('home', {
+				title : 'Control Panel',
+				countries : CT,
+				udata : req.session.user
+			});
+		}
+	},
+	
+	home_post : function(req, res){
+		if (req.session.user == null){
+			res.redirect('/');
+		}	else{
+			AM.updateAccount({
+				id		: req.session.user._id,
+				name	: req.body['name'],
+				email	: req.body['email'],
+				pass	: req.body['pass'],
+				country	: req.body['country']
+			}, function(e, o){
+				if (e){
+					res.status(400).send('error-updating-account');
+				}	else{
+					req.session.user = o;
+			// update the user's login cookies if they exists //
+					if (req.cookies.user != undefined && req.cookies.pass != undefined){
+						res.cookie('user', o.user, { maxAge: 900000 });
+						res.cookie('pass', o.pass, { maxAge: 900000 });	
+					}
+					res.status(200).send('ok');
+				}
+			});
+		}
+	},
+
+	logout_post : function(req, res){
+		res.clearCookie('user');
+		res.clearCookie('pass');
+		req.session.destroy(function(e){ res.status(200).send('ok'); });
+	},
+	
+// creating new accounts //
+	
+	novo_get : function(req, res) {
+		res.render('signup', {  title: 'Signup', countries : CT });
+	},
+	
+	novo_post : function(req, res){
+		AM.addNewAccount({
+			name 	: req.body['name'],
+			email 	: req.body['email'],
+			user 	: req.body['user'],
+			pass	: req.body['pass'],
+			country : req.body['country']
+		}, function(e){
+			if (e){
+				res.status(400).send(e);
+			}	else{
+				res.status(200).send('ok');
+			}
+		});
+	},
+
+// password reset //
+
+	semsenha_post : function(req, res){
+	// look up the user's account via their email //
+		AM.getAccountByEmail(req.body['email'], function(o){
+			if (o){
+				EM.dispatchResetPasswordLink(o, function(e, m){
+				// this callback takes a moment to return //
+				// TODO add an ajax loader to give user feedback //
+					if (!e){
+						res.status(200).send('ok');
+					}	else{
+						for (k in e) console.log('ERRO: ', k, e[k]);
+						res.status(400).send('não foi possível resetar a senha');
+					}
+				});
+			}	else{
+				res.status(400).send('email-not-found');
+			}
+		});
+	},
+
+	resetsenha_get: function(req, res) {
+		var email = req.query["e"];
+		var passH = req.query["p"];
+		AM.validateResetLink(email, passH, function(e){
+			if (e != 'ok'){
+				res.redirect('/');
+			} else{
+	// save the user's email in a session instead of sending to the client //
+				req.session.reset = { email:email, passHash:passH };
+				res.render('reset', { title : 'Resetar Senha' });
+			}
+		})
+	},
+	
+	resetsenha_post: function(req, res) {
+		var nPass = req.body['pass'];
+	// retrieve the user's email from the session to lookup their account and reset password //
+		var email = req.session.reset.email;
+	// destory the session immediately after retrieving the stored email //
+		req.session.destroy();
+		AM.updatePassword(email, nPass, function(e, o){
+			if (o){
+				res.status(200).send('ok');
+			}	else{
+                res.status(400).send('não foi possível alterar a senha');
+			}
+		})
+	},
+	
+// view & delete accounts //
+	
+	print_get: function(req, res) {
+		AM.getAllRecords( function(e, accounts){
+			res.render('print', { title : 'Account List', accts : accounts });
+		})
+	},
+	
+	deletar_post: function(req, res) {
+		AM.deleteAccount(req.body.id, function(e, obj){
+			if (!e){
+				res.clearCookie('user');
+				res.clearCookie('pass');
+				req.session.destroy(function(e){ res.status(200).send('ok'); });
+			}	else{
+				res.status(400).send('record not found');
+			}
+	    });
+	},
+	
+	reset_get: function(req, res) {
+		AM.delAllRecords(function(){
+			res.redirect('/print');	
+		});
+	}
+};
